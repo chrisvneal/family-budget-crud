dac5b193fa16c0f7c954739c5618e4a6125eb610
Chris Neal <chrisvneal@gmail.com>
2026-01-13 09:03:36 -1000

    Fix fetchCreditors

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,12 @@ import CreditorTable from "./CreditorTable";
 import api from "./apis/api";
 
 export default function App() {
-	const creditorList = [
-		{
-			id: 1,
-			name: "Household Bank",
-			payDate: 15,
-			amountDue: 30.0,
-		},
-		{
-			id: 2,
-			name: "Capital One",
-			payDate: 15,
-			amountDue: 30.0,
-		},
-	];
-
 	const [creditors, setCreditors] = useState([]);
 
 	const fetchCreditors = async () => {
-		try {
-			const response = await api.get("/creditors");
-			// console.log(response.data);
-			setCreditors(response.data);
-		} catch (error) {
-			console.error("Error fetching creditors:", error);
-		}
+		const response = await api.get("/creditors");
+
+		setCreditors(response.data);
 	};
 
 	useEffect(() => {
