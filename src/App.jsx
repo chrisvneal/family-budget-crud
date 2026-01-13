3e9fa2bc1572578ed0cc7acb0f4d1c6af71253d4
Chris Neal <chrisvneal@gmail.com>
2026-01-13 09:59:05 -1000

    Add handleSubmit & handlechange

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ import api from "./apis/api";
 
 export default function App() {
 	const [creditors, setCreditors] = useState([]);
+	const [form, setForm] = useState({
+		name: "",
+		amountDue: "",
+		payDate: "",
+	});
 
 	const fetchCreditors = async () => {
 		const response = await api.get("/creditors");
@@ -11,24 +16,38 @@ export default function App() {
 		setCreditors(response.data);
 	};
 
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+
+		const res = await api.post("/creditors", {
+			name: form.name,
+			amountDue: Number(form.amountDue),
+			payDate: Number(form.payDate),
+		});
+
+		setCreditors((prev) => [...prev, res.data]);
+
+		setForm({ name: "", amountDue: "", payDate: "" });
+	};
+
+	const handleChange = (e) => {
+		setForm((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+	};
+
 	useEffect(() => {
 		fetchCreditors();
 	}, []);
-
-	const handleSubmit = (e) => {
-		e.preventDefault();
-	};
 
 	return (
 		<div className='App'>
 			<form onSubmit={handleSubmit}>
 				<h2>Add Creditor</h2>
 				<label>Name</label>
-				<input type='text' name='name' />
+				<input type='text' name='name' onChange={handleChange} value={form.name} />
 				<label>Amount Due</label>
-				<input type='number' name='amountDue' />
+				<input type='number' name='amountDue' onChange={handleChange} value={form.amountDue} />
 				<label>Pay Date</label>
-				<input type='number' name='payDate' />
+				<input type='number' name='payDate' onChange={handleChange} value={form.payDate} />
 				<button type='submit'>Add Creditor</button>
 			</form>
 			<h2>Creditors</h2>
